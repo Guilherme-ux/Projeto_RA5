3d5c00b1500e7c1e4bc25b30a4bf8b2bbe560119
Guilherme-ux <soares.guilherme332gmail.com>
2022-10-15 16:45:05 -0300

    configurando pastas e ambientes

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log('Hello World !');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/Projeto_RA5',{
+    useUnifiedTopology:true,
+    useNewUrlParser:true,
+    UsefindAndModify:false
+},function(err){
+   if(err){
+        console.log(err)
+   }else{
+        console.log('MongoDB Conectado com sucesso!')     
+   } 
+});
+
+const app = express ();
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.get('/',function(req,res){
+    res.json({message:'Hello World'});
+});
+
+app.listen(port,function(){
+    console.log('Server runing on port ${port}')
+})
